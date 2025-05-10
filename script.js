7950355f22ed65896212c02f6f46f6137346dc3a
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-05-10 12:00:20 +0530

    set up api

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-let city = document.querySelector("#city").value;
 let btn = document.querySelector(".btn");
-let cityName = document.querySelector(".cityName");
+let cityName = document.querySelector("#cityName");
+let temperature = document.querySelector("#temperature");
+let feelsLike = document.querySelector("#feelsLike");
+let humidity = document.querySelector("#humidity");
+let lon = document.querySelector("#lon");
+let lat = document.querySelector("#lat");
+let description = document.querySelector("#description");
+let weatherIcon = document.querySelector("#weatherIcon");
 
+async function getWeather(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city").value;
 
+  const apiKey = `11efeb0579bec011f50442a3abb6b746`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&units=metric&appid=${apiKey}`;
 
+  var response = await fetch(apiUrl);
+  var data = await response.json();
+  console.log(data);
 
-async function getWeather() {
-    const apiKey = `11efeb0579bec011f50442a3abb6b746`;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&units=metric&appid=${apiKey}`;
-    var response = await fetch(apiUrl);
-    var data = await response.json();
-    console.log(data);
-
-    console.log(data.main.temp);
-    cityName.innerText = city;
+  cityName.innerText = data.name;
+  temperature.innerText = `${data.main.temp}°C`;
+  feelsLike.innerText = `${data.main.feels_like}`;
+  humidity.innerHTML = `${data.main.humidity}%`;
+  lon.innerText = `${data.coord.lon}`;
+  lat.innerText = `${data.coord.lat}`;
+  description.innerText = `${data.weather[0].description}`
+  weatherIcon.src = weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
 }
 
-btn.addEventListener('click' , getWeather);
+btn.addEventListener("click", getWeather);
 
 // getWeather();
-
